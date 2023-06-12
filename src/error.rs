203456319c0cac8cdb7d9c77d9f690d225bfa4c7
Luis Dietl <interruptedThread@gmail.com>
2023-06-12 19:29:11 +0200

    Added interactive mode

---
+++ src/error.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+#[derive(Debug)]
+pub enum AppError {
+    IoError(io::Error),
+}
+
+impl std::error::Error for AppError {
+
+}
+
+impl std::fmt::Display for AppError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[Error]: {}", match self {
+            Self::IoError(err) => format!("IoError: {}", err),
+        })
+    }
+}
+
+impl From<std::io::Error> for AppError {
+    fn from(value: std::io::Error) -> Self {
+        Self::IoError(value)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use error::AppError;
+
+mod error;
+
 struct Lexer {
     input: Vec<char>,
     position: usize,
@@ -178,35 +182,60 @@ impl Lexer {
 
 }
 
-fn main() {
-    let input = "
-        let five = 5;\n
-        let ten = 10;\n
-        let add = fn(x, y) {\n
-            x + y;\n
-        };\n
-        let result = add(five, ten);
+impl Iterator for Lexer {
+    type Item = Token;
 
-        !-/*5;
-        5< 10 > 5;
-
-        if (5 < 10) {
-            return true;
-        } else {
-            return false;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.next_token() {
+            Token::EOF => None,
+            token => Some(token),
         }
-
-        10 == 10;
-        10 != 9;
-    ".to_string();
-
-    let mut lexer = Lexer::new(input);
-    let mut token = lexer.next_token();
-    loop {
-        println!("{:?}", token);
-        if token == Token::EOF {
-            break;
-        }
-        token = lexer.next_token();
     }
+}
+
+fn start_interactive() -> Result<(), AppError> {
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+
+        let mut lexer = Lexer::new(input);
+        for token in lexer {
+            println!("{:?}", token);
+        }
+    }
+}
+
+fn main() -> Result<(), AppError>{
+    // let input = "
+    //     let five = 5;\n
+    //     let ten = 10;\n
+    //     let add = fn(x, y) {\n
+    //         x + y;\n
+    //     };\n
+    //     let result = add(five, ten);
+    //
+    //     !-/*5;
+    //     5< 10 > 5;
+    //
+    //     if (5 < 10) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    //
+    //     10 == 10;
+    //     10 != 9;
+    // ".to_string();
+    //
+    // let mut lexer = Lexer::new(input);
+    // let mut token = lexer.next_token();
+    // loop {
+    //     println!("{:?}", token);
+    //     if token == Token::EOF {
+    //         break;
+    //     }
+    //     token = lexer.next_token();
+    // }
+    
+    start_interactive()
 }
